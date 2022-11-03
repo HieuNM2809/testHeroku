0bd7eafd9231515cfb1e6d6a078f2e0c5e25f0e2
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-11-03 10:05:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -52,8 +52,9 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log('listening on *:' + port);
 });
 
 
